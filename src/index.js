0afd913d4298840939062a6486618eaf952ae117
Sankalp Saxena <sankalp1632000@gmail.com>
2021-04-25 18:20:37 +0530

    Add changes to App test

--- src/index.js
+++ src/index.js
@@ -2,16 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: process.env.REACTIVE_POD_API_KEY,
+    authDomain: process.env.REACTIVE_POD_AUTH_DOMAIN,
+    databaseURL: "https://reactive-pod.firebaseio.com",
+    projectId: "reactive-pod",
+    storageBucket: "reactive-pod.appspot.com",
+    messagingSenderId: process.env.REACTIVE_POD_MESSAGEING_SENDER_ID,
+    appId: process.env.REACTIVE_POD_APP_ID
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+ReactDOM.render(<App />,document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
